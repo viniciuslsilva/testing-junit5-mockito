f4d20f1095626f8bf4d17d8aacc5cf8daddba74b
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-08 08:26:42 -0300

    Adding mock annotation example

---
+++ src/test/java/guru/springframework/sfgpetclinic/AnnotationMocksTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+public class AnnotationMocksTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+}
