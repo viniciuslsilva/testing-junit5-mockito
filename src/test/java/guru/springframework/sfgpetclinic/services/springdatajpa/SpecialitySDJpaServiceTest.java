170b50db79b87573e91a788d85656e9c45e5f919
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-09 22:39:01 -0300

    Adding demo of arg matches

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -24,6 +24,15 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
+    void testDeleteByObject() {
+        Speciality speciality = new Speciality();
+
+        service.delete(speciality);
+
+        verify(specialtyRepository).delete(any(Speciality.class));
+    }
+
+    @Test
     void findByIdTest() {
         Speciality speciality = new Speciality();
 
@@ -33,7 +42,7 @@ class SpecialitySDJpaServiceTest {
 
         assertThat(foundSpeciality).isNotNull();
 
-        verify(specialtyRepository).findById(1l);
+        verify(specialtyRepository).findById(anyLong());
     }
 
     @Test
